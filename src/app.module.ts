4434973daad9fbe3db7a3b9889592ef9874701c1
Angel <angelloayzah2016@gmail.com>
2025-05-04 12:51:14 -0500

    Add VideoResultsModule to AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { VideoModule } from './modules/video/video.module';
 import { SeederModule } from './modules/seeder/seeder.module';
+import { VideoResultsModule } from './modules/video-results/video-results.module';
 
 @Module({
-  imports: [VideoModule, SeederModule],
+  imports: [VideoModule, SeederModule, VideoResultsModule],
   controllers: [AppController],
   providers: [AppService],
 })

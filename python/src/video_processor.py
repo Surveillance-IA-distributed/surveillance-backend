559cc93645b0dffb5f7446db6509b5512eb75031
Angel <angelloayzah2016@gmail.com>
2025-05-05 02:50:27 -0500

    Refactor video processing logic to filter out already processed videos and improve output clarity

--- python/src/video_processor.py
+++ python/src/video_processor.py
@@ -7,7 +7,23 @@ def process_video(video_path, output_folder):
     process_motion(video_path, output_folder)
 
 def process_videos_in_folder(input_folder, output_folder):
-    videos = [f for f in os.listdir(input_folder) if f.endswith(('.mp4', '.avi', '.mov'))]
+    videos = [
+        f for f in os.listdir(input_folder) 
+        if f.endswith(('.mp4', '.avi', '.mov'))
+    ]
+
+    # Obtener los nombres de las carpetas ya creadas en el output_folder
+    processed_video_names = set(os.listdir(output_folder))
+
+    # Filtrar videos que ya han sido procesados
+    videos_to_process = [
+        video for video in videos 
+        if os.path.splitext(video)[0] not in processed_video_names
+    ]
+
+    print(f"🔍 Videos nuevos por procesar: {videos_to_process}")
 
     with ThreadPoolExecutor() as executor:
-        executor.map(lambda video: process_video(os.path.join(input_folder, video), output_folder), videos)
+        executor.map(lambda video: process_video(
+            os.path.join(input_folder, video), output_folder), videos_to_process
+        )
